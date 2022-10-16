296862559802bb81a7384e5b45d99f91b20f8d7e
Elvira <esinyanskaya@gmail.com>
2022-10-16 21:47:32 +0300

    Finishing section 'About us'

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-var mixer = mixitup('.directions__list');
+$(function () {
+  var mixer = mixitup('.directions__list');
 
+  $(document).on('click', '.directions__filter-btn', function () {
+    $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+    $(this).addClass('directions__filter-btn--active');
+  
+  })
 
+})
