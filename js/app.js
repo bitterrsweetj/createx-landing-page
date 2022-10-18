1e5a8c3dbd1061dc91e8607e9ebdfeab2d0f7658
Elvira <esinyanskaya@gmail.com>
2022-10-18 23:42:30 +0300

    Adding slider section

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@ $(function () {
   $(document).on('click', '.directions__filter-btn', function () {
     $('.directions__filter-btn').removeClass('directions__filter-btn--active')
     $(this).addClass('directions__filter-btn--active');
-  
+
   })
 
+  $('.team__slider').slick({
+    slidesToShow: 4,
+    infinite: true,
+    draggable: false,
+  })
 })
