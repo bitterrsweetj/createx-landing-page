55c14b780ae77e374de2ab4637bc8ad54ddd23eb
Elvira <esinyanskaya@gmail.com>
2022-10-20 01:36:44 +0300

    Adding arrows for slider section

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
-$(function () {
-  var mixer = mixitup('.directions__list');
+var mixer = mixitup('.directions__list');
 
-  $(document).on('click', '.directions__filter-btn', function () {
-    $('.directions__filter-btn').removeClass('directions__filter-btn--active')
-    $(this).addClass('directions__filter-btn--active');
+$(document).on('click', '.directions__filter-btn', function () {
+  $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+  $(this).addClass('directions__filter-btn--active');
 
-  })
-
-  $('.team__slider').slick({
-    slidesToShow: 4,
-    infinite: true,
-    draggable: false,
-  })
 })
+
+$('.team__slider').slick({
+  arrows: false,
+  slidesToShow: 4,
+  infinite: true,
+  draggable: false,
+  waitForAnimate: false,
+})
+
+$('.team__slider-prev').on('click', function (e) {
+  e.preventDefault()
+  $('.team__slider').slick('slickPrev')
+})
+$('.team__slider-next').on('click', function (e) {
+  e.preventDefault()
+  $('.team__slider').slick('slickNext')
+})
+
