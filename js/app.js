fd76985d6ef94d2ae825b40439ab67d5ba3006b6
Elvira <esinyanskaya@gmail.com>
2022-10-24 17:33:54 +0300

    Adding background to slider sections

--- js/app.js
+++ js/app.js
@@ -23,3 +23,21 @@ $('.team__slider-next').on('click', function (e) {
   $('.team__slider').slick('slickNext')
 })
 
+
+$('.testimonials__slider').slick({
+  arrows: false,
+  dots:true,
+  appendDots: $(".testimonials__dots"),
+  slidesToShow: 1,
+  infinite: true,
+  waitForAnimate: false,
+})
+
+$('.testimonials__prev').on('click', function (e) {
+  e.preventDefault()
+  $('.testimonials__slider').slick('slickPrev')
+})
+$('.testimonials__next').on('click', function (e) {
+  e.preventDefault()
+  $('.testimonials__slider').slick('slickNext')
+})
